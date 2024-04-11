3e30966887aebfd74111a6e1ce9da7864a6b00ff
Kamau-sam <skamau.6411@gmail.com>
2024-04-11 14:17:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(name, email) {
+  const newUser = {
+    name: name,
+    email: email,
+  };
+  const configObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(newUser),
+  };
+  return fetch("http://localhost:3000/users", configObject)
+    .then((res) => res.json())
+    .then((data) => {
+      document.body.innerHTML += data.id;
+      console.log(data);
+    })
+    .catch((error) => {
+      document.body.innerHTML += error.message;
+    });
+}
